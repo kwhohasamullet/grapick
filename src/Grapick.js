1f5fae423396a76aa9815ec54fa5e2390f05b912
Kieran Hutchinson <kwhohasamullet@gmail.com>
2021-10-04 20:49:01 +0800

    Colour value of new handlers now Hex to match all other colour values given by default colour picker

--- src/Grapick.js
+++ src/Grapick.js
@@ -456,9 +456,9 @@ export default class Grapick extends EventEmitter {
       context.fillStyle = grad;
       context.fillRect(0, 0, canvas.width, canvas.height);
       canvas.style.background = 'black';
-      const rgba = canvas.getContext('2d').getImageData(x, y, 1, 1).data;
-      const color = `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${rgba[3]})`;
-      const fc = color == 'rgba(0, 0, 0, 0)' ? opt.emptyColor : color;
+      const colourValues = canvas.getContext('2d').getImageData(x, y, 1, 1).data;
+      const color = `#${colourValues[0].toString(16)}, ${colourValues[1].toString(16)}, ${colourValues[2].toString(16)}`;
+      const fc = color == '#000000' ? opt.emptyColor : color;
       this.addHandler(percentage, fc);
     });
   }
